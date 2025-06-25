83b23dc76ff6fee8025c7a6e49dc7ad39ec9c46d
Dhanyasri-m <dhanyasri.marimuthu@gmail.com>
2025-06-25 15:13:00 +0530

    Navbar created

--- Frontent/src/App.jsx
+++ Frontent/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Dashboard from '/Users/dharunprasath/Documents/Kalki/LunaSurface AI/Frontent/src/Components/Dashboard.jsx'
+import Dashboard from './Components/Dashboard.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
